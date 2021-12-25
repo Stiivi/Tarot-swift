19a16cdc31359ff6c0ac6442e1cd7f3dd273685a
Stefan Urbanek <stefan@agentfarms.net>
2021-12-25 22:36:42 +0100

    Object as identifiable
    - made object identifyable
    - better object description

--- Sources/GraphMemory/Object.swift
+++ Sources/GraphMemory/Object.swift
@@ -14,7 +14,7 @@ public typealias PropertyValue = String
 /// An object in a graph memory. It represents one of two graph components:
 /// a node or a link. Objects can store information
 ///
-open class Object {
+open class Object: Identifiable {
     /// Graph memory that the object is associated with.
     ///
     var graph: GraphMemory?
@@ -32,8 +32,9 @@ open class Object {
     
     /// Create an empty object. The object needs to be associated with a memory.
     ///
-    public init(id: OID?=nil) {
+    public init(id: OID?=nil, attributes: [String:Value]=[:]) {
         self.id = id
+        self.attributes = attributes
     }
 
     public subscript(_ key:String) -> Value? {
@@ -41,6 +42,7 @@ open class Object {
             return attributes[key]
         }
         set(value) {
+            // #FIXME: Notify memory delegate
             attributes[key] = value
         }
     }
@@ -57,11 +59,12 @@ extension Object: Hashable {
     }
 }
 
-extension Object: CustomDebugStringConvertible {
-    public var debugDescription: String {
-        var items = attributes.map { "\($0.key): \($0.value)" }
-        var joined = items.joined(separator: ", ")
+extension Object: CustomStringConvertible {
+    public var description: String {
+        let items = attributes.map { "\($0.key): \($0.value)" }
+        let joined = items.joined(separator: ", ")
+        let idString = id.map { String($0) } ?? "nil"
         
-        return "Object{\(joined)}"
+        return "Object(id: \(idString), attributes: [\(joined)])"
     }
 }
