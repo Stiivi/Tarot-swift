65d48f5f85d630b5b6edc8f3514c72f9a0afd32a
Stefan Urbanek <stefan@agentfarms.net>
2021-12-26 01:02:47 +0100

    Change to ArgumentParser, remove references to cards
    - Changed the tool to use ArgumentParser
    - Added data reference into the package
    - Removed static references to data
    - Added Space initialization from a package
    - Refactored Package a bit to get particular resource references (similar to Bundle)
    - Loader uses the new package instead of separate package + data root

---
+++ Sources/GraphMemory/Package.swift
@@ -0,0 +1,201 @@
+//
+//  File.swift
+//  
+//
+//  Created by Stefan Urbanek on 2021/11/6.
+//
+import Foundation
+import System
+
+/// Description of a link resource.
+///
+/// The ``resource`` property refers to a resource, for example a file, the
+/// links are stored. ``namespace`` specifies in which namespace the
+/// references to objects will be resolved. If not provided, then default shared
+/// namespace will be used.
+///
+/// Optional ``label`` is used for user-facing descriptive label of the resource that
+/// might be friendlier and more informative than the resource name.
+///
+public struct LinkResourceDescription: Codable {
+    let label: String?
+    let resource: String
+    let namespace: String?
+
+    public init(resource: String, namespace: String?=nil, label: String?=nil) {
+        self.resource = resource
+        self.namespace = namespace
+        self.label = label
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        let label = try container.decodeIfPresent(String.self, forKey: .label)
+        let resource = try container.decode(String.self, forKey: .resource)
+        let namespace = try container.decodeIfPresent(String.self, forKey: .namespace)
+
+        self.init(resource: resource, namespace: namespace, label: label )
+    }
+}
+
+/// Description of a node resource.
+///
+/// The ``resource`` property refers to a resource, for example a file, the
+/// nodes are stored. ``namespace`` specifies in which namespace the
+/// objects keys will be stored and later resolved when loading links.
+/// If not provided, then default shared namespace will be used.
+///
+/// Optional ``label`` is used for user-facing descriptive label of the resource that
+/// might be friendlier and more informative than the resource name.
+///
+public struct NodeResourceDescription: Codable {
+    let label: String?
+    let resource: String
+    
+    // FIXME: Rename to `tag` with free interpretation of the semantic layer
+    let trait: String?
+    let namespace: String?
+    
+    public var description: String {
+        var desc: String
+        if let label = label {
+            desc = "\(label) in \(resource)"
+        }
+        else {
+            desc = resource
+        }
+        
+        if let trait = trait {
+            desc += "as \(trait)"
+        }
+        
+        return desc
+    }
+}
+
+/// Description of a package with tabular data, typically CSV files, which
+/// contain nodes and links.
+///
+public class PackageInfo: Decodable {
+    /// Human-readable label of the package.
+    let label: String
+    
+    /// List of resources containing nodes.
+    let nodes: [NodeResourceDescription]
+    
+    /// List of resources containing links.
+    let links: [LinkResourceDescription]
+    
+    /// Mapping of field names in the tabular resources.
+    let fieldMap: FieldMap?
+    
+    /// Path to the resources. If not provided then the package path is used.
+    let resourcesPath: FilePath?
+    
+    enum CodingKeys: CodingKey {
+        case label
+        case nodes
+        case links
+        case fieldMap
+        case resourcesPath
+    }
+    
+    public required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        label = try values.decode(String.self, forKey: .label)
+        nodes = try values.decode([NodeResourceDescription].self, forKey: .nodes)
+        links = try values.decode([LinkResourceDescription].self, forKey: .links)
+        fieldMap = try values.decodeIfPresent(FieldMap.self, forKey: .fieldMap)
+        
+        let path = try values.decode(String.self, forKey: .resourcesPath)
+        resourcesPath = FilePath(path)
+    }
+}
+
+/// Graph Memory file package.
+///
+/// This class describes a collection of resources and metadata about a graph memory
+/// stored in files.
+///
+/// Package is a directory. The content of the directory is:
+///
+/// - `info.json`: information about the package (required)
+/// - `model.json`: model of the package (optional)
+/// - resource files containing nodes
+/// - resource files containing links
+///
+/// Currently only tabular data files (CSV) are supported as resource files.
+///
+public class Package {
+    let packageRoot: URL
+
+    let info: PackageInfo
+    
+    /// Human-readable label of the package.
+    var label: String { info.label }
+    
+    /// List of resources containing nodes.
+    var nodes: [NodeResourceDescription] { info.nodes }
+    
+    /// List of resources containing links.
+    var links: [LinkResourceDescription] { info.links }
+    
+    /// Mapping of field names in the tabular resources.
+    var fieldMap: FieldMap? { info.fieldMap }
+        
+    /// Base URL for resources.
+    ///
+    var resourcesURL: URL {
+        if let path = info.resourcesPath {
+            if path.isRelative {
+                let url: URL
+                url = packageRoot.appendingPathComponent(path.string,
+                                                         isDirectory: true)
+                return url
+            }
+            else {
+                return URL(fileURLWithPath: path.string, isDirectory: true)
+            }
+        }
+        else {
+            return packageRoot
+        }
+    }
+    
+    /// Returns URL for a resource containing the model.
+    ///
+    public var modelURL: URL {
+        return packageRoot.appendingPathComponent("model.json")
+    }
+    
+    /// Create a package from given URL.
+    ///
+    /// The package is a directory with at least one file: `info.json`. The
+    /// typical package structure is:
+    ///
+    /// ```
+    /// PACKAGE/
+    ///     info.json
+    ///     model.json
+    ///     nodes.csv
+    ///     links.csv
+    /// ```
+    ///
+    public init(url: URL) throws {
+        packageRoot = url
+        
+        let infoURL = packageRoot.appendingPathComponent("info.json")
+        let json = try Data(contentsOf: infoURL)
+        info = try JSONDecoder().decode(PackageInfo.self, from: json)
+    }
+    
+    /// Return a full URL for a resource. The method does not check whether the
+    /// resource exists or not.
+    ///
+    public func url(forResource resource: String) -> URL {
+        return resourcesURL.appendingPathComponent(resource)
+    }
+
+}
+
--- Sources/Tarot/main.swift
+++ Sources/Tarot/main.swift
@@ -6,15 +6,17 @@
 //
 import GraphMemory
 import Foundation
+import ArgumentParser
 
-let dataURL = URL(fileURLWithPath: "/Users/stefan/Documents/Data Cards/Cards of Data Governance 2021",
-                  isDirectory: true)
 
-func main() throws {
-    let tool: TarotTool
-    
+/// Create a space from a package specified in the options.
+///
+func makeSpace(options: Options) -> Space {
+    let space: Space
+    let packageURL = URL(fileURLWithPath: options.packagePath, isDirectory: true)
+
     do {
-        tool = try TarotTool(dataURL: dataURL)
+        space = try Space(packageURL: packageURL)
     }
     catch ImportError.validationError(let issues) {
         for issue in issues {
@@ -22,15 +24,91 @@ func main() throws {
         }
         fatalError("Validation errors found. Abandoning.")
     }
+    catch {
+        fatalError("Unable to create space: \(error)")
+    }
     
-    // Validate and load data
-    print("Loading data...")
-    tool.writeDOT()
-   
-    // print("Generating PDFs...")
-    // tool.makeCardsPDF()
-    // FIXME: VALIDATE NODES!!!!
-    // FIXME: VALIDATE MODEL CONFORMANCE!!!
+    return space
 }
 
-try main()
+
+// The Command
+// ------------------------------------------------------------------------
+
+struct Tarot: ParsableCommand {
+    static var configuration = CommandConfiguration(
+        abstract: "Tarot utility.",
+        subcommands: [
+            List.self,
+            Print.self,
+            WriteDOT.self,
+        ],
+        defaultSubcommand: List.self)
+}
+
+struct Options: ParsableArguments {
+    @Option(name: [.long, .customShort("p")], help: "Path to a package directory")
+    var packagePath = "Data.tarot"
+}
+
+extension Tarot {
+    struct List: ParsableCommand {
+        static var configuration
+            = CommandConfiguration(abstract: "List nodes.")
+
+        @OptionGroup var options: Options
+
+        mutating func run() {
+            let space = makeSpace(options: options)
+            for node in space.memory.nodes {
+                let traitName = node.trait?.name ?? "no trait"
+                print("\(node.id!)(\(traitName))")
+            }
+        }
+    }
+}
+
+extension Tarot {
+    struct Print: ParsableCommand {
+        static var configuration
+            = CommandConfiguration(abstract: "Print an object by reference.")
+
+        @OptionGroup var options: Options
+
+        @Argument(help: "Object identifier")
+        var reference: Int
+
+        mutating func run() {
+            let space = makeSpace(options: options)
+
+            print("Node: \(reference)")
+            
+            guard let object = space.memory.object(reference) else {
+                fatalError("Unknown object: \(reference)")
+            }
+
+            print("Object: \(object)")
+        }
+    }
+}
+
+
+extension Tarot {
+    struct WriteDOT: ParsableCommand {
+        static var configuration
+            = CommandConfiguration(abstract: "Write a Graphviz DOT file.")
+
+        @OptionGroup var options: Options
+        
+        @Option(name: [.long, .customShort("o")],
+                help: "Path to a DOT file where the output will be written.")
+        var output = "tarot.dot"
+
+        mutating func run() {
+            let space = makeSpace(options: options)
+            space.memory.writeDot(path: output, name: "cards")
+        }
+    }
+}
+
+Tarot.main()
